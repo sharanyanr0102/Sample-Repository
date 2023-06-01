7c9a749587cda22e824483d9631caeafd2c63e69
sharanyanr <sharanyanr0102@gmail.com>
2023-06-01 13:33:37 +0530

    Commit on secondaryBranch

---
+++ src/utils/BasePage.java
@@ -0,0 +1,13 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+	WebDriver driver;
+	
+	BasePage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+}
